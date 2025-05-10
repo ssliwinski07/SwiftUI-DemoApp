4d3bda4fb1d6b02cbfa41d0ff7817a615a0cfeb6
Szymon Sliwinski <s.sliwinski@hotmail.com>
2025-05-10 17:06:43 +0200

    Init

---
+++ SwiftUI-DemoApp/SwiftUI_DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DemoAppApp.swift
+//  SwiftUI-DemoApp
+//
+//  Created by Szymon Śliwiński on 10/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-DemoAppTests/SwiftUI_DemoAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUI_DemoAppTests.swift
+//  SwiftUI-DemoAppTests
+//
+//  Created by Szymon Śliwiński on 10/05/2025.
+//
+
+import Testing
+
+struct SwiftUI_DemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
